4f246b81fd11fa47798cfbc4656ec3434bd87bd3
gabrielaguanini <gguanini@gmail.com>
2024-10-06 16:42:14 -0300

    Deploy Render prueba zona horaria 6

--- BackEnd/delivery/src/main/java/com/delivery/delivery/DeliveryApplication.java
+++ BackEnd/delivery/src/main/java/com/delivery/delivery/DeliveryApplication.java
@@ -9,10 +9,11 @@ public class DeliveryApplication {
     
     
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {            
 		SpringApplication.run(DeliveryApplication.class, args);
                 
-                
+               
+
 	}
 
 }
